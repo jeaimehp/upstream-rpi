fa8682b60558f831baf1e4250f90b30a121d7d22
Je'aime Powell <jeaimehp@gmail.com>
2022-04-22 13:28:03 -0500

    switched to size only check

--- stengl-minio-tests/transferwavfiles-all.bash
+++ stengl-minio-tests/transferwavfiles-all.bash
@@ -15,7 +15,7 @@ echo "Sending files."
 
 #cat $SOUNDFILES_DIR/../data/sound-xfer-temp.log-$TIMESTAMP | while read line;do /usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/sendtocorral-minio.py $SOUNDFILES_DIR/$line; done
 cat $SOUNDFILES_DIR/../data/sound-xfer-temp.log-$TIMESTAMP | while read line; do
-	if [[ `(/usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/stengl-minio-md5check-cleanoutput.py $SOUNDFILES_DIR/$line)` == true ]]; then
+	if [[ `(/usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/stengl-minio-sizeonly-check-cleanoutput.py $SOUNDFILES_DIR/$line)` == true ]]; then
 	      echo "File $SOUNDFILES_DIR/$line already sync'd"
         else 
                /usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/sendtocorral-minio.py $SOUNDFILES_DIR/$line
